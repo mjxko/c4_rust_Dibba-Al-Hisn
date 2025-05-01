36459f7f427e67f6aeaa587234a6d51255a125ff
Sark1764 <sera.alkaabi171@gmail.com>
2025-05-02 00:44:00 +0400

    comments on token.rs

--- src/token.rs
+++ src/token.rs
@@ -1,41 +1,66 @@
-// token.rs
-
+// Define all possible tokens we might find in the source code
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum Token {
-    Num(i64),
-    Id(String),
-    Fun, Sys, Glo, Loc,
+    // Literal values
+    Num(i64),            // Number, like 42
+    Id(String),          // Identifier, like variable or function name
+
+    // Classes or scopes
+    Fun, Sys, Glo, Loc,  // Function, System, Global, Local
+
+    // Keywords
     Char, Else, Enum, If, Int, Return, Sizeof, While,
-    Assign, Cond, Lor, Lan, Or, Xor, And, Eq, Ne, Lt, Gt,
-    Le, Ge, Shl, Shr, Add, Sub, Mul, Div, Mod, Inc, Dec, Brak,
-    LParen, RParen, Semicolon,
+
+    // Operators and symbols
+    Assign,     // =
+    Cond,       // ?
+    Lor, Lan,   // || and &&
+    Or, Xor, And,
+    Eq, Ne,     // == and !=
+    Lt, Gt, Le, Ge,     // <, >, <=, >=
+    Shl, Shr,   // << and >>
+    Add, Sub, Mul, Div, Mod,
+    Inc, Dec,   // ++ and --
+
+    Brak,       // [
+    LParen,     // (
+    RParen,     // )
+    Semicolon,  // ;
+
+    // Function-like token
     Printf,
+
+    // Any unknown or unsupported character
     Unknown(char),
+
+    // End of input
     Eof,
 }
 
-
+// This enum represents the data types in our language
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Type {
-    Char,
-    Int,
-    Ptr,
+    Char,   // character type
+    Int,    // integer type
+    Ptr,    // pointer type
 }
 
+// This enum tells us the role or kind of a symbol (like a variable or function)
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Class {
-    Num,
-    Fun,
-    Sys,
-    Glo,
-    Loc,
-    None,
+    Num,    // constant number
+    Fun,    // function
+    Sys,    // system function
+    Glo,    // global variable
+    Loc,    // local variable
+    None,   // not set
 }
 
+// Helper function to return the name of a token as a string
 pub fn token_name(token: &Token) -> &'static str {
     match token {
-        Token::Num(_) => "Num",
-        Token::Id(_) => "Id",
+        Token::Num(_) => "Num",               // Number
+        Token::Id(_) => "Id",                 // Identifier
         Token::Fun => "Fun",
         Token::Sys => "Sys",
         Token::Glo => "Glo",
@@ -48,6 +73,8 @@ pub fn token_name(token: &Token) -> &'static str {
         Token::Return => "Return",
         Token::Sizeof => "Sizeof",
         Token::While => "While",
+
+        // Operators and symbols as string versions
         Token::Assign => "=",
         Token::Cond => "?",
         Token::Lor => "||",
@@ -74,9 +101,9 @@ pub fn token_name(token: &Token) -> &'static str {
         Token::LParen => "(",
         Token::RParen => ")",
         Token::Semicolon => ";",
+
         Token::Printf => "printf",
         Token::Unknown(_) => "Unknown",
         Token::Eof => "EOF",
     }
 }
-
